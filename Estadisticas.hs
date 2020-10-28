3acbbbe62c3ce085edb3bdafbfeed7122ab79a8b
randycj <randyconejo@gmail.com>
2020-10-28 11:45:58 -0600

    se empieza con top de parqueos

--- Estadisticas.hs
+++ Estadisticas.hs
@@ -1,34 +1,35 @@
 module Estadisticas where
 import CargaUsuarios
-
+import CargaParqueos
+-- TOP USUARIOS
 topUsuarios u a = do
-    listaTOP <- obtenerTOP u a []
+    listaTOP <- obtenerTOPUsuarios u a []
     return ()
 
-insertarUsuario :: [[String]] -> [String] -> Integer -> [[String]]
-insertarUsuario lTOP datos contador = do
+insertarUsuario :: [[String]] -> [String] -> [[String]]
+insertarUsuario lTOP datos = do
     if null(lTOP) then
         [datos]
     else
         if (read (datos!!1) :: Integer) > (read ((head lTOP)!!1) :: Integer) then
-            do
             [datos] ++ lTOP
         else
-            [(head lTOP)] ++ insertarUsuario (tail lTOP) datos (contador + 1)
+            [(head lTOP)] ++ insertarUsuario (tail lTOP) datos
 
 
-obtenerTOP :: [Usuario] -> [Alquiler] -> [[String]] -> IO [[String]]
-obtenerTOP [] _ lTOP = lTOP
-obtenerTOP lU lA lTOP = do
+obtenerTOPUsuarios :: [Usuario] -> [Alquiler] -> [[String]] -> IO [[String]]
+obtenerTOPUsuarios [] lA lTOP = return lTOP
+obtenerTOPUsuarios lU lA lTOP = do
     let largoLista = length lTOP
     let cantidad = obtenerAlquileresXUsuario (head lU) lA 0
+    let nombreUsurio = getNombreUsuario (head lU)
     if largoLista == 0 then
         do
-        obtenerTOP (tail lU) lA [[getNombreUsuario (head lU), show cantidad]]
+        obtenerTOPUsuarios (tail lU) lA [[nombreUsuario, show cantidad]]
     else
         do
-        let listaTOP = insertarUsuario lTOP [getNombreUsuario (head lU), cantidad] 0
-        obtenerTOP (tail lU) lA listaTOP
+        let listaTOP = insertarUsuario lTOP [nombreUsuario, show cantidad]
+        obtenerTOPUsuarios (tail lU) lA listaTOP
         
 
 obtenerAlquileresXUsuario :: Usuario -> [Alquiler] -> Integer -> Integer
@@ -42,3 +43,39 @@ obtenerAlquileresXUsuario usuario lA cantidad = do
         obtenerAlquileresXUsuario usuario (tail lA) (cantidad + 1)
     else
         obtenerAlquileresXUsuario usuario (tail lA) cantidad
+
+-- TOP PARQUEOS
+topParqueos p a = do
+    listaTOP <- obtenerTOPParqueos p a []
+    return ()
+
+obtenerTOPParqueos :: [Parqueo] -> [Alquiler] -> [[String]] -> IO [[String]]
+obtenerTOPParqueos [] _ lTOP = return lTOP
+obtenerTOPParqueos lP lA lTOP = do
+    let largoLista = length lTOP
+    let cantidad = obtenerViajesXParqueo (head lP) lA 0
+    let nombreParqueo = getNombreParqueo (head lP)
+    if largoLista == 0 then
+        do
+        obtenerTOPParqueos (tail lP) lA [[nombreParqueo, show cantidad]]
+    else
+        do
+        let listaTOP = insertarUsuario lTOP [nombreParqueo, show cantidad]
+        obtenerTOPParqueos (tail lP) lA listaTOP
+        
+
+obtenerViajesXParqueo :: Parqueo -> [Alquiler] -> Integer -> Integer
+obtenerViajesXParqueo p [] cantidad = cantidad
+obtenerViajesXParqueo parqueo lA cantidad = do
+    let 
+        nombre = getNombreParqueo parqueo
+        parqueoSalida = getParqueoSalida (head lA)
+        parqueoLlegada = getParqueoLlegada (head lA)
+
+    if nombre == parqueoSalida then
+        obtenerViajesXParqueo parqueo (tail lA) (cantidad + 1)
+    else
+        if nombre == parqueoLlegada then
+            obtenerViajesXParqueo parqueo (tail lA) (cantidad + 1)
+        else
+            obtenerViajesXParqueo parqueo (tail lA) cantidad
